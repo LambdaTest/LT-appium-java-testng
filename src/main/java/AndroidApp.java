05aec2251697a11eb25bd0870f939c5e56b97bdf
Private1647 <shantanuw@lambdatest.com>
2022-04-25 16:32:55 +0530

    Added webdriverWait to remove test flakiness

--- src/main/java/AndroidApp.java
+++ src/main/java/AndroidApp.java
@@ -12,8 +12,8 @@ import java.util.List;
 
 public class AndroidApp {
 
-    String userName = System.getenv("LT_USERNAME") == null ? "username" : System.getenv("LT_USERNAME");
-    String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "accesskey" : System.getenv("LT_ACCESS_KEY");
+    String userName = System.getenv("LT_USERNAME") == null ? "shantanuw" : System.getenv("LT_USERNAME");
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "rsS1I3BEWoWcAUJHtfa5TZaLjRV1NTeKmyRVJ8QjtCHhMIK3BW" : System.getenv("LT_ACCESS_KEY");
 
     public String gridURL = "@beta-hub.lambdatest.com/wd/hub";
 
@@ -24,14 +24,14 @@ public class AndroidApp {
     public void AndroidApp1(String device, String version, String platform) {
         try {
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("build","Proverbial Android");
+            capabilities.setCapability("build","Java TestNG Android");
             capabilities.setCapability("name",platform+" "+device+" "+version);
             capabilities.setCapability("deviceName", device);
             capabilities.setCapability("platformVersion",version);
             capabilities.setCapability("platformName", platform);
             capabilities.setCapability("isRealMobile", true);
             //AppURL (Create from Wikipedia.apk sample in project)
-            capabilities.setCapability("app", "lt://APP10020211648185973280934"); //Enter your app url
+            capabilities.setCapability("app", "lt://APP100202371650862145999377"); //Enter your app url
             capabilities.setCapability("deviceOrientation", "PORTRAIT");
             capabilities.setCapability("console", true);
             capabilities.setCapability("network", false);
@@ -45,6 +45,7 @@ public class AndroidApp {
             MobileElement color = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/color");
             //Changes color
             color.click();
+            Thread.sleep(2000);
             //Back to black color
             color.click();
 
@@ -59,6 +60,7 @@ public class AndroidApp {
             //notification is visible
             MobileElement notification = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/notification");
             notification.click();
+            Thread.sleep(2000);
 
             //Open the geolocation page
             MobileElement geo = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/geoLocation");
@@ -73,21 +75,20 @@ public class AndroidApp {
             MobileElement speedtest = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/speedTest");
             speedtest.click();
             Thread.sleep(5000);
-            //MobileElement el10 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.widget.RelativeLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View[3]/android.view.View[1]/android.view.View/android.widget.Button");
-            //el10.click();
-            //Thread.sleep(25000);
 
-
-            MobileElement el11 = (MobileElement) driver.findElementByXPath("//android.widget.FrameLayout[@content-desc=\"Home\"]/android.widget.FrameLayout/android.widget.ImageView");
-            el11.click();
+            MobileElement Home = (MobileElement) driver.findElementByAccessibilityId("Home");
+            Home.click();
 
             //Opens the browser
             MobileElement browser = (MobileElement) driver.findElementByAccessibilityId("Browser");
             browser.click();
-            MobileElement el13 = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/url");
-            el13.sendKeys("https://www.lambdatest.com");
-            MobileElement el14 = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/find");
-            el14.click();
+
+            MobileElement url = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/url");
+            url.sendKeys("https://www.lambdatest.com");
+
+            MobileElement find = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/find");
+            find.click();
+
             driver.quit();
 
         } catch (Exception e) {
