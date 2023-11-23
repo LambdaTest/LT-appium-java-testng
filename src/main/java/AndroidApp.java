8614f392adc747fd04776bf9853ec15ca7778e20
PARAG JAIN <87814011+paragjain15@users.noreply.github.com>
2023-11-23 20:54:00 +0530

    Added support for calling Dynamic App_ID

--- src/main/java/AndroidApp.java
+++ src/main/java/AndroidApp.java
@@ -12,96 +12,99 @@ import java.util.List;
 
 public class AndroidApp {
 
-    String userName = System.getenv("LT_USERNAME") == null ?
-            "username" : System.getenv("LT_USERNAME"); //Add username here
-    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
-            "accessKey" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
+  String userName = System.getenv("LT_USERNAME") == null ?
+    "username" :
+    System.getenv("LT_USERNAME"); //Add username here
+  String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
+    "accessKey" :
+    System.getenv("LT_ACCESS_KEY"); //Add accessKey here
 
-    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+  String app_id = System.getenv("LT_APP_ID") == null ?
+    "lt://proverbial-android" :
+    System.getenv("LT_APP_ID");      //Enter your LambdaTest App ID at the place of lt://proverbial-android
+  String grid_url = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");
 
-    AppiumDriver driver;
+  AppiumDriver driver;
 
-    @Test
-    @org.testng.annotations.Parameters(value = {"device", "version", "platform"})
-    public void AndroidApp1(String device, String version, String platform) {
-        try {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("build","Java TestNG Android");
-            capabilities.setCapability("name",platform+" "+device+" "+version);
-            capabilities.setCapability("deviceName", device);
-            capabilities.setCapability("platformVersion",version);
-            capabilities.setCapability("platformName", platform);
-            capabilities.setCapability("isRealMobile", true);
-            //AppURL (Create from Wikipedia.apk sample in project)
-            capabilities.setCapability("app", "lt://proverbial-android"); //Enter your app url
-            capabilities.setCapability("deviceOrientation", "PORTRAIT");
-            capabilities.setCapability("console", true);
-            capabilities.setCapability("network", false);
-            capabilities.setCapability("visual", true);
-            capabilities.setCapability("devicelog", true);
-            //capabilities.setCapability("geoLocation", "HK");
+  @Test
+  @org.testng.annotations.Parameters(value = { "device", "version", "platform" })
+  public void AndroidApp1(String device, String version, String platform) {
+    try {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setCapability("build", "Java TestNG");
+      capabilities.setCapability("name", platform + " " + device + " " + version);
+      capabilities.setCapability("deviceName", device);
+      capabilities.setCapability("platformVersion", version);
+      capabilities.setCapability("platformName", platform);
+      capabilities.setCapability("isRealMobile", true);
+      //AppURL (Create from Wikipedia.apk sample in project)
+      capabilities.setCapability("app", app_id); //Enter your app url
+      capabilities.setCapability("deviceOrientation", "PORTRAIT");
+      capabilities.setCapability("network", false);
+      capabilities.setCapability("visual", true);
+      capabilities.setCapability("devicelog", true);
+      //capabilities.setCapability("geoLocation", "HK");
 
-            String hub = "https://" + userName + ":" + accessKey + gridURL;
-            driver = new AppiumDriver(new URL(hub), capabilities);
+      String hub = "https://" + userName + ":" + accessKey + "@" + grid_url + "/wd/hub";
+      driver = new AppiumDriver(new URL(hub), capabilities);
 
-            MobileElement color = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/color");
-            //Changes color to pink
-            color.click();
-            Thread.sleep(1000);
-            //Back to orginal color
-            color.click();
+      MobileElement color = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/color");
+      //Changes color to pink
+      color.click();
+      Thread.sleep(1000);
+      //Back to orginal color
+      color.click();
 
-            MobileElement text = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/Text");
-            //Changes the text to "Proverbial"
-            text.click();
+      MobileElement text = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/Text");
+      //Changes the text to "Proverbial"
+      text.click();
 
-            //toast will be visible
-            MobileElement toast = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/toast");
-            toast.click();
+      //toast will be visible
+      MobileElement toast = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/toast");
+      toast.click();
 
-            //notification will be visible
-            MobileElement notification = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/notification");
-            notification.click();
-            Thread.sleep(2000);
+      //notification will be visible
+      MobileElement notification = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/notification");
+      notification.click();
+      Thread.sleep(2000);
 
-            //Opens the geolocation page
-            MobileElement geo = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/geoLocation");
-            geo.click();
-            Thread.sleep(5000);
+      //Opens the geolocation page
+      MobileElement geo = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/geoLocation");
+      geo.click();
+      Thread.sleep(5000);
 
-            //takes back to home page
-            MobileElement home = (MobileElement) driver.findElementByAccessibilityId("Home");
-            home.click();
+      //takes back to home page
+      MobileElement home = (MobileElement) driver.findElementByAccessibilityId("Home");
+      home.click();
 
-            //Takes to speed test page
-            MobileElement speedtest = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/speedTest");
-            speedtest.click();
-            Thread.sleep(5000);
+      //Takes to speed test page
+      MobileElement speedtest = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/speedTest");
+      speedtest.click();
+      Thread.sleep(5000);
 
-            MobileElement Home = (MobileElement) driver.findElementByAccessibilityId("Home");
-            Home.click();
+      MobileElement Home = (MobileElement) driver.findElementByAccessibilityId("Home");
+      Home.click();
 
-            //Opens the browser
-            MobileElement browser = (MobileElement) driver.findElementByAccessibilityId("Browser");
-            browser.click();
+      //Opens the browser
+      MobileElement browser = (MobileElement) driver.findElementByAccessibilityId("Browser");
+      browser.click();
 
-            MobileElement url = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/url");
-            url.sendKeys("https://www.lambdatest.com");
+      MobileElement url = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/url");
+      url.sendKeys("https://www.lambdatest.com");
 
-            MobileElement find = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/find");
-            find.click();
+      MobileElement find = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/find");
+      find.click();
 
-            driver.quit();
+      driver.quit();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            try{
-                driver.quit();
-            }catch(Exception e1){
-                e.printStackTrace();
-            }
-        }
-
-
+    } catch (Exception e) {
+      e.printStackTrace();
+      try {
+        driver.quit();
+      } catch (Exception e1) {
+        e.printStackTrace();
+      }
     }
+
+  }
 }
--- src/main/java/iOSApp.java
+++ src/main/java/iOSApp.java
@@ -7,96 +7,99 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.net.URL;
+
 import org.testng.annotations.Test;
 
 public class iOSApp {
 
-    String userName = System.getenv("LT_USERNAME") == null ?
-            "username" : System.getenv("LT_USERNAME"); //Add username here
-    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
-            "accessKey" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
+  String userName = System.getenv("LT_USERNAME") == null ?
+    "username" :
+    System.getenv("LT_USERNAME"); //Add username here
+  String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
+    "accessKey" :
+    System.getenv("LT_ACCESS_KEY"); //Add accessKey here
+  String app_id = System.getenv("LT_APP_ID") == null ?
+    "lt://proverbial-ios" :
+    System.getenv("LT_APP_ID");      //Enter your LambdaTest App ID at the place of lt://proverbial-android
+  String grid_url = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");
 
-    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+  AppiumDriver driver;
 
-    AppiumDriver driver;
+  @Test
+  @org.testng.annotations.Parameters(value = { "device", "version", "platform" })
+  public void iOSApp1(String device, String version, String platform) {
 
-    @Test
-    @org.testng.annotations.Parameters(value = {"device", "version", "platform"})
-    public void iOSApp1(String device, String version, String platform) {
+    try {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setCapability("build", "Java TestNG");
+      capabilities.setCapability("name", platform + " " + device + " " + version);
+      capabilities.setCapability("deviceName", device);
+      capabilities.setCapability("platformVersion", version);
+      capabilities.setCapability("platformName", platform);
+      capabilities.setCapability("isRealMobile", true);
+      capabilities.setCapability("app", app_id); //Enter your app url
+      capabilities.setCapability("network", false);
+      capabilities.setCapability("visual", true);
+      capabilities.setCapability("devicelog", true);
+      //capabilities.setCapability("geoLocation", "HK");
 
-        try {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("build","Java TestNG iOS");
-            capabilities.setCapability("name",platform+" "+device+" "+version);
-            capabilities.setCapability("deviceName", device);
-            capabilities.setCapability("platformVersion",version);
-            capabilities.setCapability("platformName", platform);
-            capabilities.setCapability("isRealMobile", true);
-            capabilities.setCapability("app", "lt://proverbial-ios"); //Enter your app url
-            capabilities.setCapability("deviceOrientation", "PORTRAIT");
-            capabilities.setCapability("console", true);
-            capabilities.setCapability("network", false);
-            capabilities.setCapability("visual", true);
-            capabilities.setCapability("devicelog", true);
-            //capabilities.setCapability("geoLocation", "HK");
+      String hub = "https://" + userName + ":" + accessKey + "@" + grid_url + "/wd/hub";
+      driver = new AppiumDriver(new URL(hub), capabilities);
 
-            String hub = "https://" + userName + ":" + accessKey + gridURL;
-            driver = new AppiumDriver(new URL(hub), capabilities);
+      WebDriverWait Wait = new WebDriverWait(driver, 30);
 
-            WebDriverWait Wait = new WebDriverWait(driver,30);
+      //Changes the color of the text
+      Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("color"))).click();
+      Thread.sleep(1000);
 
-            //Changes the color of the text
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("color"))).click();
-            Thread.sleep(1000);
+      //Changes the text to "Proverbial"
+      Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Text"))).click();
+      Thread.sleep(1000);
 
-            //Changes the text to "Proverbial"
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Text"))).click();
-            Thread.sleep(1000);
+      //Toast will be visible
+      Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("toast"))).click();
+      Thread.sleep(1000);
 
-            //Toast will be visible
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("toast"))).click();
-            Thread.sleep(1000);
+      //Notification will be visible
+      Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("notification"))).click();
+      Thread.sleep(4000);
 
-            //Notification will be visible
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("notification"))).click();
-            Thread.sleep(4000);
+      //Opens the geolocation page
+      Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("geoLocation"))).click();
+      Thread.sleep(4000);
 
-            //Opens the geolocation page
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("geoLocation"))).click();
-            Thread.sleep(4000);
+      //Takes back
+      driver.navigate().back();
 
-            //Takes back
-            driver.navigate().back();
+      //Takes to speedtest page
+      Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("speedTest"))).click();
+      Thread.sleep(4000);
 
-            //Takes to speedtest page
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("speedTest"))).click();
-            Thread.sleep(4000);
+      driver.navigate().back();
 
-            driver.navigate().back();
+      //Opens the browser
+      Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Browser"))).click();
+      Thread.sleep(1000);
 
-            //Opens the browser
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Browser"))).click();
-            Thread.sleep(1000);
+      MobileElement url = (MobileElement) driver.findElementByAccessibilityId("url");
+      url.click();
+      url.sendKeys("https://www.lambdatest.com");
 
-            MobileElement url = (MobileElement) driver.findElementByAccessibilityId("url");
-            url.click();
-            url.sendKeys("https://www.lambdatest.com");
+      Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("find"))).click();
+      Thread.sleep(1000);
 
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("find"))).click();
-            Thread.sleep(1000);
+      driver.quit();
 
-            driver.quit();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            try{
-                driver.quit();
-            }catch(Exception e1){
-                e.printStackTrace();
-            }
-        }
-
-
+    } catch (Exception e) {
+      e.printStackTrace();
+      try {
+        driver.quit();
+      } catch (Exception e1) {
+        e.printStackTrace();
+      }
     }
+
+  }
 }
